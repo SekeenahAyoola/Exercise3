cc81f232362406ff02861bd37a93fe8d05a52f4e
SekeenahAyoola <ayoolasekeenah@gmail.com>
2023-04-09 10:05:43 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿
+// Exercise 3
+// number1
+//Console.WriteLine("Enter a number to show whether it us even or odd");
+//Console.WriteLine("TRUE for EVEN amd FALSE for ODD ");
+//Console.WriteLine("Enter your number");
+//int number = Convert.ToInt32(Console.ReadLine());
+//bool even = number%2== 0;
+//Console.WriteLine(even);
+//number2
+// Console.WriteLine("Check if a number is divisible by both 5 and 7");
+// Console.WriteLine("Enter your number");
+// Console.WriteLine("TRUE for DIVISIBLE by 5 and 7 amd FALSE for NOT DIVISIBLE BY 5 AND 7 ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// bool canDivide = num % 35 == 0;
+// Console.WriteLine(canDivide);
+// number3
+// Console.WriteLine("Input a number ");
+// int figure = Convert.ToInt32(Console.ReadLine());
+// int divideBy100 = figure /100;
+// bool thirdDigit = divideBy100 % 10 == 7;
+// Console.WriteLine(thirdDigit); 
+// number 5
+// Console.WriteLine("lets solve the area of a trapezium when given all the sides");
+// Console.WriteLine("Put in only the value of a in meters");
+// int aValue=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Now put in the value of b also in meters");
+// int bValue=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Now put in the value of h also in meters");
+// int hValue=Convert.ToInt32(Console.ReadLine());
+// int areaOfTrapezium=((aValue + bValue) *(hValue))/2;
+// Console.WriteLine($"The area of the trapezium is {areaOfTrapezium} squareunit");
+// number 6
+// Console.WriteLine("lets find the value of the area and perimeter of a rectangle");
+// Console.Write("Enter the breath of the rectangle:");
+// int breath=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the lenght of the rectangle:");
+// int lenghtOfRectangle=Convert.ToInt32(Console.ReadLine());
+// int area=(breath*lenghtOfRectangle);
+// int perimeter=((lenghtOfRectangle+breath)*2);
+// Console.WriteLine($"The area of the rectangle is {area} sqmeters \n The perimeter is {perimeter} meters.");
+// number 7
+// Console.WriteLine("Let us help find your approximate weight on the moon using your weight on earth ");
+// Console.Write("Enter your weigth: ")
+// double weightOnEarth=Convert.ToDouble(Console.ReadLine());
+//double weightOnMoon=(weightOnEarth*(0.17));
+//Console.WriteLine($"Your approximate weight on the moon is {weightOnMoon}kg");
+// number 8
+// Console.WriteLine("Lets check if a given point (x,y) is inside a circle with center (0,0) and radius 5");
+// Console.WriteLine("input your x value");
+// int xValue=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input the value of y");
+// int yValue=Convert.ToInt32(Console.ReadLine());
+// int checkXAxix=(xValue * xValue);
+// int checkYAxix=(yValue * yValue);
+// bool checkIfInsideCircle=((checkXAxix + checkYAxix) <= 25);
+// Console.WriteLine($"The Point ({xValue},{yValue}) is in the circle if TRUE and FALSE for otherwise.");
+// Console.WriteLine(checkIfInsideCircle);
+// mumber 9
+// Console.WriteLine("Lets check if a given point (x,y) is inside a circle with center (0,0) and radius 5 and outside a rectangle {(-1,1),(5,5)}");
+// Console.WriteLine("input your x value");
+// int xValue1=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input the value of y");
+// int yValue1=Convert.ToInt32(Console.ReadLine());
+// int checkXAxix1=(xValue1 * xValue1);
+// int checkYAxix1=(yValue1 * yValue1);
+// bool checkIfInsideCircle1=((checkXAxix1 + checkYAxix1) <= 25);
+// bool checkIfOutsideRectangle= (-1>= xValue1 && -5 <= xValue1) && (-5 <= yValue1 && 1>= yValue1);
+// bool isInsideCircleAndOutsIdeRectangle = checkIfInsideCircle1 && checkIfOutsideRectangle;
+// Console.WriteLine($"The Point ({xValue1},{yValue1}) is in the circle and outside rectangle if TRUE and FALSE for otherwise.");
+// Console.WriteLine(isInsideCircleAndOutsIdeRectangle);
+// number10
+// Console.WriteLine("input a four digit number only");
+// int fourDigitNumber=Convert.ToInt32(Console.ReadLine());
+// int forthDigit = (fourDigitNumber%10);
+// int thirdDigit = ((fourDigitNumber/10)%10);
+// int secondDigit = ((fourDigitNumber/100)%10);
+// int firstDigit = ((fourDigitNumber/1000)%10);
+// int sum = (firstDigit + secondDigit + thirdDigit + forthDigit);
+// Console.WriteLine($"{firstDigit}{secondDigit}{thirdDigit}{forthDigit}");
+// Console.WriteLine($"the sum of the numbers is {sum}");
+// Console.WriteLine($"{forthDigit}{thirdDigit}{secondDigit}{firstDigit}");
+// Console.WriteLine($"{forthDigit}{firstDigit}{secondDigit}{thirdDigit}");
+//  Console.WriteLine($"{firstDigit}{thirdDigit}{secondDigit}{forthDigit}");
+
+
